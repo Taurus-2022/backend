edd057e703853309d7700a2a22804dc625d0e2f8
peanut996 <peanut996@qq.com>
2022-07-30 22:24:53 +0800

    init go scf project.

---
+++ src/httpserver.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	host_ = "0.0.0.0"
+	port_ = 20000
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		_, err := rw.Write([]byte("Hello World"))
+		if err != nil {
+			log.Fatal("Error writing to client", err)
+			return
+		}
+	})
+	log.Println("server starting...")
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host_, port_), nil)
+	if err != nil {
+		log.Fatal("setup server fatal:", err)
+	}
+
+}
