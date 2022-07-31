61594b52b38d2b4a108620047b3f667071b4e66e
peanut996 <849421294godw@gmail.com>
2022-07-31 14:52:05 +0800

    [feat] add request and validation.

--- src/api/handler/signature.go
+++ src/api/handler/signature.go
@@ -4,9 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"taurus-backend/api/request"
 	"taurus-backend/api/response"
+	"taurus-backend/constant"
 )
 
 func CreateSignature(c *gin.Context) {
+	r := &request.CreateSignatureCountRequest{}
+	err := c.ShouldBindJSON(r)
+	if err != nil {
+		c.AbortWithStatusJSON(400, response.GetErrorResponse(constant.ErrorHttpParamInvalid, nil))
+	}
 	c.String(200, "success")
 }
 
--- src/api/request/signature.go
+++ src/api/request/signature.go
@@ -3,3 +3,8 @@ package request
 type GetSignatureCountRequest struct {
 	Street string `form:"street"`
 }
+
+type CreateSignatureCountRequest struct {
+	Phone  string `json:"phone" binding:"required"`
+	Street string `json:"street" binding:"required"`
+}
