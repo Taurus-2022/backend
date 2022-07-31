95e521bc721b8380b7112268d4b1ba431a7e8829
peanut996 <849421294godw@gmail.com>
2022-08-01 01:54:18 +0800

    [feat] add non null limit.

--- sql/signature.sql
+++ sql/signature.sql
@@ -3,7 +3,6 @@ CREATE TABLE `signature`
     `id`          BIGINT AUTO_INCREMENT Primary Key,
     `phone`       VARCHAR(36)  NOT NULL COMMENT '用户手机号',
     `street`      VARCHAR(255) NOT NULL NULL COMMENT '街道',
-    `sign_time`   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '签名时间',
     `create_time` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `deleted`     TINYINT(1)            DEFAULT 0 COMMENT '是否被删除',
